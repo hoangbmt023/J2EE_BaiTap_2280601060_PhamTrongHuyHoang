0c0bbf2edaa1dbe793e757948a44df9d1ca9a70b
hoangbmt023 <hoangkutehbcd@gmail.com>
2026-01-19 21:07:31 +0700

    Initial Spring Boot project

---
+++ src/main/java/com/example/j2ee_bai1/J2eeBai1Application.java
@@ -0,0 +1,12 @@
+package com.example.j2ee_bai1;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class J2eeBai1Application {
+
+	public static void main(String[] args) {
+		System.err.println("Hello Spring Boot");
+	}
+
+}
